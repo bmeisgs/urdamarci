a627fbeba257790dc948e2f7ea2d9fc3bd1852fa
Lenovo <Lenovo@192.168.0.46>
2018-05-31 13:47:14 +0100

    remove

---
+++ variables.js
@@ -0,0 +1,12 @@
+
+let word = 2
+let world = (2 && 3) ? true: false;
+let words = "win" 
+let array =[ word,world,words]
+let namedarray = {"word":word, "world":world, "words":words};
+    
+  
+        
+	
+
+
