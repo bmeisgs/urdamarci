d224608e716a18fee3f05d65e44d604c3e3b518c
Lenovo <Lenovo@192.168.0.46>
2018-06-07 07:55:32 +0100

    remove

--- variables.js
+++ variables.js
@@ -1,12 +1,15 @@
 
-let word = 2
+let word = 2;
 let world = (2 && 3) ? true: false;
-let words = "win" 
-let array =[ word,world,words]
+let words = "win" ;
 let namedarray = {"word":word, "world":world, "words":words};
-    
-  
-        
-	
+ 	
+var array =["word","world","words"];
+array.reverse();
 
+console.log (array);
 
+for ( N = 0, i < namedarray.length, ++i);
+namedarray= namedarry.length[i];
+
+console.log(namedarray);
